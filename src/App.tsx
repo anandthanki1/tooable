16503a6c0154f0c66431035ba8a78f9cef0309e2
Anand Thanki <anandthanki18@gmail.com>
2023-09-05 23:21:08 -0700

    Added logo and app name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Sidebar from './components/Sidebar'
+
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="flex">
-      <div className={`w-72 h-screen bg-light-grey`}>Sidebar</div>
+      <Sidebar />
       <div className="p-7 text-2x1 font-semibold flex-1 h-screen">
         <h1>HomePage</h1>
       </div>
